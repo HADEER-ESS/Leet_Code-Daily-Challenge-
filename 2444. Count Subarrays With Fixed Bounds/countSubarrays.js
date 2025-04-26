da6bf8fd1ea99824caba11cf21531a4502f9509b
hadeer <hadeeressam21@gmail.com>
2025-04-27 00:31:22 +0300

    add some upgrade by adding base case if all given argument have same value by create an equation (len * (len+1))/2

--- 2444. Count Subarrays With Fixed Bounds/countSubarrays.js
+++ 2444. Count Subarrays With Fixed Bounds/countSubarrays.js
@@ -1,15 +1,22 @@
 //Solution #1
 //create a Nested Loop to get every subArray in given array
 //with O(n^2) Time Complexity 
-// solve 48 out of 53
+// solve 49 out of 53
 var countSubarrays = function (nums, minK, maxK) {
+
+    //base case
+    let len = nums.length
+    let reduce = nums.reduce((acc, curr) => acc + curr, 0)
+    if (reduce / nums[0] === len && minK === maxK && minK === nums[0]) return (len * (len + 1)) / 2
+
+
     let ans = 0;
 
-    for (let l = 0; l < nums.length; l++) {
+    for (let l = 0; l < len; l++) {
         if (nums[l] < minK || nums[l] > maxK) continue;
         let arr = [nums[l]]
         if (arr.includes(minK) && arr.includes(maxK)) ans++
-        for (let r = l + 1; r < nums.length; r++) {
+        for (let r = l + 1; r < len; r++) {
             if (nums[r] < minK || nums[r] > maxK) break;
             arr.push(nums[r])
             if (arr.includes(minK) && arr.includes(maxK)) ans++
