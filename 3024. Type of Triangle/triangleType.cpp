ce4c1314146ec1e8e467f54d715770629cec6d09
hadeer <hadeeressam21@gmail.com>
2025-05-19 13:41:29 +0300

    leetcode dialy challange problem no.3024 easy problem anout Math and array, solved in O(1) time complexity using javascript and cpp

---
+++ 3024. Type of Triangle/triangleType.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        //equilateral => when all side are equal
+    if(nums[0] == nums[1] && nums[1] == nums[2]) return "equilateral";
+
+    //none => when the sum of two side smaller than the third side
+    if(
+        nums[0] + nums[1] <= nums[2] || 
+        nums[1] + nums[2] <= nums[0] ||
+        nums[0] + nums[2] <= nums[1]) return "none";
+
+    //isosceles => when two sides are equal
+    if(
+        (nums[0] == nums[1] ||
+        nums[1] == nums[2] ||
+        nums[0] == nums[2] )&&
+        (nums[0] + nums[1] > nums[2] ||
+        nums[0] + nums[2] > nums[1] ||
+        nums[1] + nums[1] > nums[0])
+    ) return "isosceles";
+
+    //scalene => all sides are different but the sum of each two side greater than the third one
+    return "scalene";
+    }
+};
